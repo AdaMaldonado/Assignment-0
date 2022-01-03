462d2654f7871605be46902c6d5d20e13086a63d
Ada Maldonado <adagigimaldonado@gmail.com>
2022-01-03 01:33:21 -0500

    completed

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,15 @@
 function frequencyCounter(word) {
   // Insert code here;
+  let counter = {};
+  let letters = word.split("");
+  for (let i = 0; i < letters.length; i++) {
+    if (counter[letters[i]]) {
+      counter[letters[i]]++;
+    } else {
+      counter[letters[i]] = 1;
+    }
+  }
+  return counter;
 }
 
 // Do not edit this line;
