0aed373ed65073b21d773fbc41af6648a32e060f
Ada Maldonado <adagigimaldonado@gmail.com>
2022-01-03 02:02:47 -0500

    completed

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,12 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+  let union = new Set();
+  for (let i = 0; i < args.length; i++) {
+    for (let j of args[i]) {
+      union.add(j);
+    }
+  }
+  return union;
 }
 
 // Do not edit this line;
