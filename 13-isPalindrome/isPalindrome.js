3ff4af2d7db807781ef387dcd23b508f190dc46b
Ada Maldonado <adagigimaldonado@gmail.com>
2022-01-03 01:12:19 -0500

    completed

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,15 @@
 function isPalindrome(word) {
   // Insert code here;
+  let letters = word.split("");
+  let isPal = false;
+  for (let i = 0; i < letters.length/2 ; i++) {
+    if (letters[i] === letters[letters.length - i - 1]) {
+      isPal = true;
+    } else {
+      return false;
+    }
+  } 
+  return isPal;
 }
 
 // Do not edit this line;
