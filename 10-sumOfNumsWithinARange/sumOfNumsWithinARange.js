6b205fb9c6a3edab7b3e425c22ab35d1e55d1757
Ada Maldonado <adagigimaldonado@gmail.com>
2022-01-03 00:37:21 -0500

    completed

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,14 @@
 function sumOfNumsWithinARange(nums, start, end) {
   // Insert code here;
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = start; j <= end; j++) {
+      if (nums[i] === j) {
+        count++;
+      }
+    }
+  }
+  return count;
 }
 
 // Do not edit this line;
