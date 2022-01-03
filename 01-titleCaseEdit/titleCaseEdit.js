9a4c942e0f6f0978ec7660524705e81c528b2774
Ada Maldonado <adagigimaldonado@gmail.com>
2022-01-02 23:58:51 -0500

    completed

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,10 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  let splitTitle = title.split(' ');
+  for (let i = 0; i < splitTitle.length; i++) {
+    splitTitle[i] = splitTitle[i].charAt(0).toUpperCase() + splitTitle[i].substr(1);
+  }
+  return splitTitle.join(' ');
 }
 
 // Do not edit this line;
